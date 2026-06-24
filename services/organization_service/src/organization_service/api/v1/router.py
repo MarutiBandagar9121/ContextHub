4664df0f25e8720331816be84196ac5a97e92904
MarutiBandagar9121 <marut.bandagar9121@gmail.com>
2026-06-24 11:00:54 +0530

    fix: Chnage made to health route of org service

--- services/organization_service/src/organization_service/api/v1/router.py
+++ services/organization_service/src/organization_service/api/v1/router.py
@@ -1,10 +1,16 @@
 from fastapi import APIRouter
+
 from organization_service.api.v1.organization_routes import router as organization_routes
+from organization_service.config.settings import settings
 
 router = APIRouter()
 
 @router.get("/health")
-def get_hello():
-    return {"msg":"Organization service is healthy"}
+def health_check():
+    return {
+        "msg":"Auth service is healthy", 
+        "app_name": settings.app_name, 
+        "app_version": settings.app_version
+        }
 
 router.include_router(organization_routes, prefix="/organization", tags=["organization"])
